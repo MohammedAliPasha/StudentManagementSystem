dadaf6c22794234e6bc3f69e46ccb6e675804edd
MohammedAliPasha <111505193+MohammedAliPasha@users.noreply.github.com>
2022-10-25 12:01:12 +0530

    Add files via upload

---
+++ stdmss/DataBaseConnection.java
@@ -0,0 +1,37 @@
+package stdmss;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+
+import java.sql.SQLException;
+
+ import javax.swing.JOptionPane;
+
+public class DataBaseConnection {
+public static Connection dbConn() {
+
+String url = "jdbc:mysql://localhost:3306/studentdatamanagement";
+
+String user ="root";
+
+String pass ="Alipasha@9";
+
+try {
+
+Connection conn = DriverManager.getConnection(url,user,pass);
+
+return conn;
+
+
+} catch (SQLException e) {
+
+JOptionPane.showMessageDialog(null, "Connection Failed");
+
+e.printStackTrace();
+
+return null;
+
+}
+}
+}
